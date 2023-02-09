c682b7a347d94ef65ddfcba229dea347bcf79d2a
Naki <nbsp1221@gmail.com>
2023-02-09 09:04:25 +0900

    🔧 Allocate 16GB of RAM to the server

--- 1.16.5/run.sh
+++ 1.16.5/run.sh
@@ -2,7 +2,7 @@
 
 while true
 do
-  java -Xms6G -Xmx6G -jar forge-1.16.5-36.2.34.jar nogui
+  java -Xms16G -Xmx16G -jar forge-1.16.5-36.2.34.jar nogui
   echo "Restarting automatically in 10 seconds (press Ctrl + C to cancel)"
   sleep 10
 done
